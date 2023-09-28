31a4912f0b4a39d0a4eab0114c51ac48a4c8946b
Pathak1511 <hritik1511@gmail.com>
2023-09-28 07:36:06 +0530

    backend

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const server = http.createServer(app);
 
 const io = new Server(server);
 
-app.use(express.static("build"));
+app.use(express.static("public"));
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const userSocketMap = {};
